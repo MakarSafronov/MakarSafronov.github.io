a61f1511793d41c7b30ac0809d199adad885b4a7
MakarSafronov <makarsafronov1@gmail.com>
2024-12-16 13:33:35 +0300

    обновление сайта

--- myscript.js
+++ myscript.js
@@ -1,3 +1,16 @@
-var title = document.getElementById('myTitle');
-var button = document.getElementById('myButton')
-var modal = document.getElementById('mymodal')
+var nameInput = document.getElementById('name-input');
+var EmailInput= document.getElementById('email-input');
+var LaterInput = document.getElementById('later-input');
+var CleanForm = document.getElementById('clean-form');
+var WriteLater = document.getElementById('write-later');
+var returnText = document.getElementById('return-text');
+var MyClose = document.getElementById('my-Close');
+
+WriteLater.addEventListener('submit', (event) => {
+   WriteLater.reset();
+    returnText.showModal();
+})
+
+MyClose.addEventListener("click", (event) => {
+    returnText.close();
+})
